e8ce124726d19bde0fe66e9127d206034ec1feac
AlcosaC <alcosa@floridauniversitaria.es>
2023-02-07 13:01:17 +0100

    prueba 6

--- test/enanaTest.php
+++ test/enanaTest.php
@@ -24,7 +24,7 @@ class EnanaTest extends TestCase {
 
     public function testHeridaGrave() {
         $objeto = new Enana("Natalia","20","viva");
-        $this->assertEquals("viva",$objeto->heridaGrave(0));
+        $this->assertEquals("limbo",$objeto->heridaGrave(0));
         #Se probará el efecto de una herida grave a una Enana con una situación de viva.
         #Se tendrá que probar que la vida es 0 y además que su situación es limbo
 
@@ -32,7 +32,7 @@ class EnanaTest extends TestCase {
     
     public function testPocimaRevive() {
         $objeto = new Enana("Estefania","-5","muerta");
-        $this->assertEquals("muerta",$objeto->pocima(-2));
+        $this->assertEquals("viva",$objeto->pocima(-2));
         #Se probará el efecto de administrar una pócima a una Enana muerta pero con una vida mayor que -10 y menor que 0
         #Se tendrá que probar que la vida es mayor que 0 y que su situación ha cambiado a viva
 
@@ -40,7 +40,7 @@ class EnanaTest extends TestCase {
 
     public function testPocimaExtraLimbo() {
         $objeto = new Enana("Ayuso","0","limbo");
-        $this->assertEquals("limbo",$objeto->pocimaExtra(0));
+        $this->assertEquals("viva",$objeto->pocimaExtra(0));
         #Se probará el efecto de administrar una pócima Extra a una Enana en el limbo.
         #Se tendrá que probar que la vida es 50 y la situación ha cambiado a viva.
 
